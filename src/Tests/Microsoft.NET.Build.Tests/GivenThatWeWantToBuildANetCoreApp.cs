d25a731201cde6d4b5ccf8b8c39718090d2a1f56
Daniel Plaisted <daplaist@microsoft.com>
2018-02-12 17:37:52 -0800

    Move roll forward tests to separate test class

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildANetCoreApp.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildANetCoreApp.cs
@@ -8,14 +8,9 @@ using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
 using Microsoft.NET.TestFramework.ProjectConstruction;
-using Newtonsoft.Json.Linq;
-using NuGet.Common;
-using NuGet.Frameworks;
-using NuGet.ProjectModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
@@ -28,114 +23,6 @@ namespace Microsoft.NET.Build.Tests
     {
         public GivenThatWeWantToBuildANetCoreApp(ITestOutputHelper log) : base(log)
         {
-        }
-
-        [Theory]
-        //  TargetFramework, RuntimeFrameworkVersion, ExpectedPackageVersion, ExpectedRuntimeFrameworkVersion
-        [InlineData("netcoreapp1.0", null, "1.0.5", "1.0.5")]
-        [InlineData("netcoreapp1.0", "1.0.0", "1.0.0", "1.0.0")]
-        [InlineData("netcoreapp1.0", "1.0.3", "1.0.3", "1.0.3")]
-        [InlineData("netcoreapp1.1", null, "1.1.2", "1.1.2")]
-        [InlineData("netcoreapp1.1", "1.1.0", "1.1.0", "1.1.0")]
-        [InlineData("netcoreapp1.1.1", null, "1.1.1", "1.1.1")]
-        public void It_targets_the_right_shared_framework(string targetFramework, string runtimeFrameworkVersion,
-            string expectedPackageVersion, string expectedRuntimeVersion)
-        {
-            string testIdentifier = "SharedFrameworkTargeting_" + string.Join("_", targetFramework, runtimeFrameworkVersion ?? "null");
-
-            It_targets_the_right_framework(testIdentifier, targetFramework, runtimeFrameworkVersion,
-                selfContained: false, isExe: true,
-                expectedPackageVersion: expectedPackageVersion, expectedRuntimeVersion: expectedRuntimeVersion);
-        }
-
-        //  Test behavior when implicit version differs for framework-dependent and self-contained apps
-        [Theory]
-        [InlineData("netcoreapp1.0", false, true, "1.0.5")]
-        [InlineData("netcoreapp1.0", true, true, TestContext.ImplicitRuntimeFrameworkVersionForSelfContainedNetCoreApp1_0)]
-        [InlineData("netcoreapp1.0", false, false, "1.0.5")]
-        [InlineData("netcoreapp1.1", false, true, "1.1.2")]
-        [InlineData("netcoreapp1.1", true, true, TestContext.ImplicitRuntimeFrameworkVersionForSelfContainedNetCoreApp1_1)]
-        [InlineData("netcoreapp1.1", false, false, "1.1.2")]
-        [InlineData("netcoreapp2.0", false, true, "2.0.0")]
-        [InlineData("netcoreapp2.0", true, true, TestContext.ImplicitRuntimeFrameworkVersionForSelfContainedNetCoreApp2_0)]
-        [InlineData("netcoreapp2.0", false, false, "2.0.0")]
-        public void It_targets_the_right_framework_depending_on_output_type(string targetFramework, bool selfContained, bool isExe, string expectedFrameworkVersion)
-        {
-            string testIdentifier = "Framework_targeting_" + targetFramework + "_" + (isExe ? "App_" : "Lib_") + (selfContained ? "SelfContained" : "FrameworkDependent");
-
-            It_targets_the_right_framework(testIdentifier, targetFramework, null, selfContained, isExe, expectedFrameworkVersion, expectedFrameworkVersion);
-        }
-
-        private void It_targets_the_right_framework(
-            string testIdentifier,
-            string targetFramework,
-            string runtimeFrameworkVersion,
-            bool selfContained,
-            bool isExe,
-            string expectedPackageVersion,
-            string expectedRuntimeVersion,
-            string extraMSBuildArguments = null)
-        {
-            string runtimeIdentifier = null;
-            if (selfContained)
-            {
-                runtimeIdentifier = EnvironmentInfo.GetCompatibleRid(targetFramework);
-            }
-
-            var testProject = new TestProject()
-            {
-                Name = "FrameworkTargetTest",
-                TargetFrameworks = targetFramework,
-                RuntimeFrameworkVersion = runtimeFrameworkVersion,
-                IsSdkProject = true,
-                IsExe = isExe,
-                RuntimeIdentifier = runtimeIdentifier
-            };
-
-            var extraArgs = extraMSBuildArguments?.Split(' ') ?? Array.Empty<string>();
-
-            var testAsset = _testAssetsManager.CreateTestProject(testProject, testIdentifier)
-                .Restore(Log, testProject.Name, extraArgs);
-
-            var buildCommand = new BuildCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name));
-
-            buildCommand
-                .Execute(extraArgs)
-                .Should()
-                .Pass();
-
-            var outputDirectory = buildCommand.GetOutputDirectory(targetFramework, runtimeIdentifier: runtimeIdentifier);
-            if (isExe)
-            {
-                //  Self-contained apps don't write a framework version to the runtimeconfig, so only check this for framework-dependent apps
-                if (!selfContained)
-                {
-                    string runtimeConfigFile = Path.Combine(outputDirectory.FullName, testProject.Name + ".runtimeconfig.json");
-                    string runtimeConfigContents = File.ReadAllText(runtimeConfigFile);
-                    JObject runtimeConfig = JObject.Parse(runtimeConfigContents);
-
-                    string actualRuntimeFrameworkVersion = ((JValue)runtimeConfig["runtimeOptions"]["framework"]["version"]).Value<string>();
-                    actualRuntimeFrameworkVersion.Should().Be(expectedRuntimeVersion);
-                }
-
-                var runtimeconfigDevFileName = testProject.Name + ".runtimeconfig.dev.json";
-                outputDirectory.Should()
-                        .HaveFile(runtimeconfigDevFileName);
-
-                string devruntimeConfigContents = File.ReadAllText(Path.Combine(outputDirectory.FullName, runtimeconfigDevFileName));
-                JObject devruntimeConfig = JObject.Parse(devruntimeConfigContents);
-
-                var additionalProbingPaths = ((JArray)devruntimeConfig["runtimeOptions"]["additionalProbingPaths"]).Values<string>();
-                // can't use Path.Combine on segments with an illegal `|` character
-                var expectedPath = $"{Path.Combine(GetUserProfile(), ".dotnet", "store")}{Path.DirectorySeparatorChar}|arch|{Path.DirectorySeparatorChar}|tfm|";
-                additionalProbingPaths.Should().Contain(expectedPath);
-            }
-
-            LockFile lockFile = LockFileUtilities.GetLockFile(Path.Combine(buildCommand.ProjectRootPath, "obj", "project.assets.json"), NullLogger.Instance);
-
-            var target = lockFile.GetTarget(NuGetFramework.Parse(targetFramework), null);
-            var netCoreAppLibrary = target.Libraries.Single(l => l.Name == "Microsoft.NETCore.App");
-            netCoreAppLibrary.Version.ToString().Should().Be(expectedPackageVersion);
         }
 
         [Fact]
@@ -429,21 +316,6 @@ public static class Program
                 .Select(Path.GetFileName)
                 .Should()
                 .BeEquivalentTo("netcoreapp1.1");
-        }
-
-        private static string GetUserProfile()
-        {
-            string userDir;
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                userDir = "USERPROFILE";
-            }
-            else
-            {
-                userDir = "HOME";
-            }
-
-            return Environment.GetEnvironmentVariable(userDir);
         }
     }
 }
