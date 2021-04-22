0a1b528ea2b650851aafde5e3a1dd139a6b76954
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-04-21 20:56:50 -0700

    add cswinrt test

---
+++ CsWinRtTest/NativeDesktopApp/WinMain.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Coords;
+using namespace Windows::Foundation;
+
+int __stdcall wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
+{
+    init_apartment(apartment_type::single_threaded);
+    Uri uri(L"http://aka.ms/cppwinrt");
+    ::MessageBoxW(::GetDesktopWindow(), uri.AbsoluteUri().c_str(), L"C++/WinRT Desktop Application", MB_OK);
+    Coord a = Coord();
+    Coord b = Coord(39.0, 80.0);
+    ::MessageBoxW(::GetDesktopWindow(), uri.AbsoluteUri().c_str(), L"C++/WinRT Desktop Application", MB_OK);
+    auto aStr = a.ToString();
+    auto bStr = b.ToString();
+    auto aToB = a.Distance(b);
+}
